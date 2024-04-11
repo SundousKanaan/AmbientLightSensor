79c08ff347b27c88065e2ead393c6e8ed68ddfe8
Sundous Kanaan <sundous.kanaan@hva.nl>
2024-04-11 20:52:39 +0200

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,23 @@
+// Check if AmbientLightSensor is supported
+let lightLevel = document.getElementById("light-level");
+if ("AmbientLightSensor" in window) {
+  const sensor = new AmbientLightSensor();
+
+  // Update light level when the sensor value changes
+  sensor.onreading = () => {
+    if (sensor.illuminance !== null) {
+      console.log("sensor.illuminance", sensor.illuminance);
+      lightLevel.textContent = `Light Level: ${sensor.illuminance.toFixed(
+        2
+      )} lux`;
+    } else {
+      console.log("sensor.illuminance", sensor.illuminance);
+    }
+  };
+
+  // Start the sensor
+  sensor.start();
+} else {
+  // If not supported, provide a fallback
+  lightLevel.textContent = "Ambient Light Sensor not supported";
+}
