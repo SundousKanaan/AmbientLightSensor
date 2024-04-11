77bfb8194c4ccae1ef042402bfb455b9453feb0a
Sundous Kanaan <sundous.kanaan@hva.nl>
2024-04-11 22:05:08 +0200

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,6 @@
 // Check if AmbientLightSensor is supported
 let lightLevel = document.getElementById("light-level");
-let lamp = document.querySelectord("#lamp .glass");
+let lamp = document.querySelector("#lamp .glass");
 
 if ("AmbientLightSensor" in window) {
   const sensor = new AmbientLightSensor();
@@ -9,8 +9,19 @@ if ("AmbientLightSensor" in window) {
 
   sensor.onreading = () => {
     if (sensor.illuminance !== null) {
+      // De methode .toFixed(2) wordt gebruikt om het aantal decimalen te beperken tot twee cijfers na de komma.
+      lightLevel.textContent = `Light Level: ${sensor.illuminance.toFixed(
+        2
+      )} lux`;
+
       // Convert illuminance to a value between 0 and 1
       const illuminancePercentage = sensor.illuminance / 1000; // Divided by 1000 for normalization
+
+      const bodyColor = `rgba(0, 0, 0, ${illuminancePercentage})`; // R, G, B, A
+      document.documentElement.style.setProperty("--body-color", bodyColor);
+      // Set text color
+      const tekstColor = `rgba(225,225, 225, ${illuminancePercentage})`; // R, G, B, A
+      document.documentElement.style.setProperty("--tekst-color", tekstColor);
       // Set lamp color
       const lampColor = `rgba(255, 255, 0, ${illuminancePercentage})`; // R, G, B, A
       document.documentElement.style.setProperty("--lamp-color", lampColor);
@@ -18,20 +29,6 @@ if ("AmbientLightSensor" in window) {
       const shadowIntensity = illuminancePercentage * 10; // Scale up to make the effect more visible
       const boxShadowValue = `0 0 ${shadowIntensity}px ${shadowIntensity}px rgba(255, 255, 0, ${illuminancePercentage})`;
       document.getElementById("lamp").style.boxShadow = boxShadowValue;
-    }
-  };
-
-  // Update light level when the sensor value changes
-  sensor.onreading = () => {
-    if (sensor.illuminance !== null) {
-      console.log("sensor.illuminance", sensor.illuminance);
-
-      // De methode .toFixed(2) wordt gebruikt om het aantal decimalen te beperken tot twee cijfers na de komma.
-      lightLevel.textContent = `Light Level: ${sensor.illuminance.toFixed(
-        2
-      )} lux`;
-    } else {
-      console.log("sensor.illuminance", sensor.illuminance);
     }
   };
 
